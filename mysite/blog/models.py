6785ff45fa2446d1a571458d7799c7c7ef32b321
Alyssa Dong <105294624+alyssadong2006@users.noreply.github.com>
2023-06-01 20:46:25 -0400

    Update models.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -7,15 +7,11 @@ class Post(models.Model):
     """
     title = models.CharField(max_length=255)
     content = models.TextField()
-    created = models.DateTimeField(auto_now_add=True) #sets on create
-    updated = models.DateTimeField(auto_now=True) #updates on each save
-    #after this, use: python manage.py makemigrations  *to enable migrations*
-    #after, in the blog file, there should be a migrations folder with some files in it
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     
     class Meta:
-        #if you want to order the posts through the 'created' category, use ['-{the category you want to use to sort}']
         ordering = ['-created']
-
-    #use <python manage.py migrate> to apply migrations
+        
     def __str__(self):
-        return self.title
+        return str(self.title)
