ebb33f9d698a57c206772ca5235f4bbc02aecd01
Alyssa Dong <105294624+alyssadong2006@users.noreply.github.com>
2023-06-01 21:34:44 -0400

    Delete mysite directory

--- mysite/tests/mysite/test_views.py
+++
@@ -1,6 +0,0 @@
-def test_index_ok(client):
-    #Make a get request to /and store the response object
-    #using the Django test client
-    response = client.get('/')
-    #Assert that the status_code s 200 (OK)
-    assert response.status_code == 200
