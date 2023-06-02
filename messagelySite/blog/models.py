676ef23c98b8e6150c78b38f136540f6e40c1a5a
Alyssa Dong <105294624+alyssadong2006@users.noreply.github.com>
2023-06-01 21:49:28 -0400

    Update models.py

--- messagelySite/blog/models.py
+++ messagelySite/blog/models.py
@@ -1,14 +1,17 @@
+"""code models"""
 from django.db import models
 
-#Represents a blog post
 class Post(models.Model):
+    """represents a blog post"""
     title = models.CharField(max_length=255)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
+        """ordering blogs by created tag"""
         ordering = ['-created']
 
     def __str__(self):
+        """creates title for blog post"""
         return str(self.title)
